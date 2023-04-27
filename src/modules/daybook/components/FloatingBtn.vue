9b7884f6ca49f774b05a725020bdaa219cb887e8
Javier Monmany <monmany1992@gmail.com>
2023-04-27 12:45:20 +0200

    Add img and icon binding

--- src/modules/daybook/components/FloatingBtn.vue
+++ src/modules/daybook/components/FloatingBtn.vue
@@ -1,24 +1,29 @@
 <template>
   <div>
     <button class="btn btn-primary">
-        <i class="fa fa-2x fa-plus"></i>
+      <i class="fa fa-2x fa-plus" :class="icon"></i>
     </button>
   </div>
 </template>
 
 <script>
 export default {
-
-}
+  props: {
+    icon: {
+      type: String,
+      default: "fa-plus",
+    },
+  },
+};
 </script>
 
 <style lang="scss" scoped>
-button{
-    border-radius: 50%;
-    bottom: 20px;
-    height: 80px;
-    position: fixed;
-    right: 20px;
-    width: 80px;
+button {
+  border-radius: 50%;
+  bottom: 20px;
+  height: 80px;
+  position: fixed;
+  right: 20px;
+  width: 80px;
 }
 </style>
---
+++ src/modules/daybook/views/EntryView.vue
@@ -0,0 +1,57 @@
+<template>
+  <div>
+    <div class="entry-title d-flex justify-content-between p-2">
+      <div>
+        <span class="text-success fs-3 fw-bold">15</span>
+        <span class="mx-1 fs-3">Julio</span>
+        <span class="mx-4 fs-4 fw-light">2023</span>
+      </div>
+      <div>
+        <button class="btn btn-danger">
+          Borrar
+          <i class="fa fa-trash-alt"></i>
+        </button>
+        <button class="btn btn-primary">
+          Subir foto
+          <i class="fa fa-upload"></i>
+        </button>
+      </div>
+    </div>
+    <hr />
+    <div class="d-flex flex-column px-3 h-75">
+      <textarea placeholder="What happend today?"></textarea>
+    </div>
+    <FloatingBtn icon="fa-save"/>
+    <img
+      src="https://img.freepik.com/free-photo/trees-forest-backgrounds_23-2148914433.jpg?w=1380&t=st=1682590447~exp=1682591047~hmac=9749be96b208ff784fa7dd857325c0097d2bc129392a3d0584e0fa8b01949a67"
+      alt="picture"
+      class="img-thumbnail"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  components: {
+    FloatingBtn: () => import("../components/FloatingBtn"),
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+textarea {
+  font-size: 2rem;
+  border: none;
+  height: 100%;
+  &:focus {
+    outline: none;
+  }
+}
+img {
+  width: 200px;
+  position: fixed;
+  bottom: 150px;
+  right: 20px;
+  box-shadow: 0px 5px 10px rgba($color: black, $alpha: 0.2);
+}
+</style>
